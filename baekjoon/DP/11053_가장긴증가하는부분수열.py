4daa5b365be05cd49a629097f162156f351b2513
hhiyeon <hhyyeon0214@gmail.com>
2024-03-22 19:01:43 +0900

    add baekjoon 11053

--- baekjoon/DP/11053_가장긴증가하는부분수열.py
+++ baekjoon/DP/11053_가장긴증가하는부분수열.py
@@ -1,13 +1,10 @@
-#n = 6
-#arr = [10,20,10,30,20,50]
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [1]*n # 리스트로 반복되는 횟수를 저장한다.
+dp = [1] * n  # 리스트로 반복되는 횟수를 저장한다.
 for i in range(n):
-	for j in range(i):
-		if arr[i] > arr[j]:
-			dp[i] = max(dp[i], dp[j]+1)
-
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 # print(dp)
 # 1 2 1 3 2 4
 # 10의 길이 1, 20의 길이 2, 10의 길이 ...
