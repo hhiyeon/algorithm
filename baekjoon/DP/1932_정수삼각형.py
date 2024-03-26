d5dde71afc2d3cb8512d1cd31d14914c40b80c7f
hhiyeon <hhyyeon0214@gmail.com>
2024-03-26 23:53:13 +0900

    add baekjoon 1932

--- baekjoon/DP/1932_정수삼각형.py
+++ baekjoon/DP/1932_정수삼각형.py
@@ -17,10 +17,3 @@ for i in range(1, n):
             dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
 # print(dp)
 print(max(dp[n-1]))
-
-# dp[1][1] = arr[1][1]
-# dp[2][1] = dp[1][1] + arr[2][1]
-# dp[2][2] = dp[1][1] + arr[2][2]
-# dp[3][1] = dp[2][1] + arr[3][1]
-# dp[3][2] = max(dp[2][1] + arr[3][2], dp[2][2] + arr[3][3])
-# dp[3][3] = dp[2][2] + arr[3][3]
